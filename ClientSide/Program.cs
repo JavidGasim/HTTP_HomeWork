8430f0f1509fec0b97fe60840b324b78ef1bb2a7
Javid <Javid@DESKTOP-3HBTQMT>
2024-03-20 03:21:23 +0400

    Add project files.

---
+++ ClientSide/Program.cs
@@ -0,0 +1,5 @@
+﻿HttpClient client = new HttpClient();
+HttpResponseMessage response = await client.GetAsync("http://localhost:27003/");
+
+var text = await response.Content.ReadAsStringAsync();
+Console.WriteLine(text);
---
+++ ServerSide/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Net;
+
+using HttpListener listener = new HttpListener();
+
+listener.Prefixes.Add("http://localhost:27003/");
+
+listener.Start();
+
+while (true)
+{
+    var context = listener.GetContext();
+
+    _ = Task.Run(() =>
+    {
+    HttpListenerRequest request = context.Request;
+    HttpListenerResponse response = context.Response;
+
+    response.ContentType = "text/html";
+    response.Headers.Add("Content-Type", "text/html");
+    response.Headers.Add("Server", "Step");
+    response.Headers.Add("Date", DateTime.Now.ToString());
+
+    var url = request.RawUrl;
+    Console.WriteLine(url);
+
+        if (url == "/")
+        {
+            response.StatusCode = 200;
+
+            using var writer = new StreamWriter(response.OutputStream);
+
+            var index = File.ReadAllText("MySite/index.html.txt");
+            writer.Write(index);
+        }
+        else
+        {
+
+            var urls = url?.Split('/').ToList();
+
+            if (urls[1] == "MySite")
+            {
+                var files = Directory.GetFiles(urls[1]);
+
+                foreach (var file in files)
+                    Console.WriteLine(file);
+
+                response.StatusCode = 200;
+                using var writer = new StreamWriter(response.OutputStream);
+                var index = File.ReadAllText($"MySite\\{urls[2]}.txt");
+                writer.Write(index);
+            }
+        }
+    });
+}
